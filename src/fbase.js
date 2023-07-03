3db06a63896a16be451fd14ae7ffbc6e4bef2192
Minhee_GitHub <126372507+minimain@users.noreply.github.com>
2023-07-04 01:50:13 +0900

    Update fbase.js

--- src/fbase.js
+++ src/fbase.js
@@ -17,7 +17,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
-  // measurementId: "G-CQRFDXFLDY" ㅣㄹ요노
 };
 
 // Initialize Firebase
